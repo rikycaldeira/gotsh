8389fabb67f142443601c4094683a0ccffcd66cc
Ricardo Caldeira <rikycaldeira@outlook.com>
2022-02-03 10:14:09 +0000

    Change sprint prefix

--- common/constants.go
+++ common/constants.go
@@ -5,7 +5,7 @@ const DATETIME_FORMAT = "2006-01-02 15:04"
 const HOURS_IN_DAY = 24
 
 const CICD_BOARD = 1728
-const CICD_SPRINT_PREFIX = "CI/CD"
+const CICD_SPRINT_PREFIX = "[Component] CI/CD"
 const CICD_TIME_TRACKING_TASK = "Time tracking tasks placeholder"
 
 var ACTIVITY_TRANSLATION = map[string]string{
